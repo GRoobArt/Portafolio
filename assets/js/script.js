90980a7ddba54e5ef09fda31731c394be8035334
Robert Garcia <rbert.g.ch@gmail.com>
2020-06-21 13:43:46 -0400

    1.1V Añadiendo Complementos Js y mas detalles

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $("a").click(function (e) { 
+        if (this.hash !== ""){
+            e.preventDefault();
+            var gato = this.hash;
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash = gato
+            });
+        }
+    });
+    $('[data-toggle="popover"]').popover()
+})
